7f66e4a34263bb48d00ddf4521685f9a8416a481
bazeusz <bazeusz@outlook.com>
2016-07-06 15:02:39 +0200

    Adding Post, Tags and Category classes

---
+++ JustBlog.core/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustBlog.core
+{
+    public class Category
+    {
+        public virtual int Id
+        { get; set; }
+        public virtual string Name
+        { get; set; }
+        public virtual string UrlSlug
+        { get; set; }
+        public virtual string Description
+        { get; set; }
+        public virtual IList<Post> Posts
+        { get; set; }
+    }
+}
---
+++ JustBlog.core/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustBlog.core
+{
+    public class Post
+    {
+        public virtual int Id
+        { get; set; }
+        public virtual string Title
+        { get; set; }
+        public virtual string ShortDescription
+        { get; set; }
+        public virtual string Description
+        { get; set; }
+        public virtual string Meta
+        { get; set; }
+        public virtual string UrlSlug
+        { get; set; }
+        public virtual bool Published
+        { get; set; }
+        public virtual DateTime PostedOn
+        { get; set; }
+        public virtual DateTime? Modified //? means that it's nullable
+        { get; set; }
+        public virtual Category Category
+        { get; set; }
+        public virtual IList<Tag> Tags
+        { get; set; }
+    }
+}
